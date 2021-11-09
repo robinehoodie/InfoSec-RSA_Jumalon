1f1b81142386a2924c94110a20267540e8c92ffb
robinehoodie <74012464+robinehoodie@users.noreply.github.com>
2021-11-10 00:16:45 +0800

    Update InfoSec.py

--- InfoSec.py
+++ InfoSec.py
@@ -28,6 +28,18 @@ def EncryptionAndDecryption(p , q, string):
     n= p * q
     z = (p-1) * (q-1)
     x = []
+    y = []
+    
+
+    for i in range(len(string)):
+      
+        if(string[i].isupper() == True):
+            y.append(True)
+        else:
+            y.append(False)
+
+  
+
     for i in range (1,n):
         if(GCD(i,z)==1):
             e=i
@@ -38,26 +50,37 @@ def EncryptionAndDecryption(p , q, string):
              d=a
              break
 
+    print("\nValue of p*q = ",n)
+    print("Value of (p-1) * (q-1) =", z)
+    print("Value of e=",e)
+    print("Value of d=", d)
+    print("Public key (",n,",",e,")")
+    print("Private key (",n,",",d,")")
+   #Encryption
+    print("\n *************ENCRYPTION***************\n")
+#     print("Letter \t Numeric Representation \t m^e \t\t\t   Cipher Text")
   
-
-    #Encryption
-    print("\n        \t  *************ENCRYPTION***************")
-    print("Letter \t Numeric Representation \t m^e \t\t\t   Cipher Text")
-    
     for i in range(len(string)):
         s = ord(string[i].lower())-96               #convert to alphabet numbers
         x.append(s**e % n);                         #Encryption formula
-        print(" ",string[i],"\t",s,"\t",s**e,"\t",x[i])
-            
+        print(x[i], end=' ') 
+#         print(" ",string[i],"\t",s,"\t",s**e,"\t",x[i])
+    print("\n")
    #Decryption
-    print("\n      \t *************DECRYPTION***************")
-    print("Cipher Text \t\t\t c^d \t\t\t\t m= m^e mod n \t    Plane Text")
-
+    print("\n      \t *************DECRYPTION***************\n")
+#     print("Cipher Text \t\t\t c^d \t\t\t\t m= m^e mod n \t    Plane Text")
+    count = 0
     for i in x:
+        
         k= (i**d) % n 
         w = chr(k + 96)                             #Decryption formula
-        print(" ",i,"\t",i**d,"\t",(i**d)**e % n,"\t", w)
+        if(y[count]==True):
+            print(w.upper(), end='')
+        else:
+            print(w,end='')
+        count+=1
+    print("\n")
+         
+#         print(" ",i,"\t",i**d,"\t",(i**d)**e % n,"\t", w)   
 
 EncryptionAndDecryption(p,q,string)
-
-
